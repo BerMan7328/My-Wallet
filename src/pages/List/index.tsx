2fd58bcbef7cb386b6a137510eab786141bd6748
BerMan7328 <bmanoelbh@gmail.com>
2021-02-28 14:52:23 -0300

    att formatDate 3

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -6,6 +6,7 @@ import HistoryFinnanceCard from '../../components/HistoryFinnanceCard';
 import gains from '../../repositories/gains';
 import expenses from '../../repositories/expenses';
 import formatCurrency from '../../utils/formatCurrency';
+import formatDate from '../../utils/formatDate';
 
 interface IRouteParams {
     match: {
@@ -58,7 +59,7 @@ const List: React.FC<IRouteParams> = ({ match }) => {
                 description: item.description,
                 amountFormatted: formatCurrency(Number(item.amount)),
                 frequency: item.frequency,
-                dateFormatted: item.date,
+                dateFormatted: formatDate(item.date),
                 tagColor: item.frequency === 'recorrente' ? '#4E41F0' : '#E44C4E'
             }
         })
