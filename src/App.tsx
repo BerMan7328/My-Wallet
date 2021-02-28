1ae50f54225a7feaf87cbcf3e8d89d1d2a1eac2a
BerMan7328 <bmanoelbh@gmail.com>
2021-02-28 00:57:14 -0300

    Commit 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import dark from './styles/themes/dark';
-import Layout from './components/Layout';
 import GlobalStyles from './styles/GlobalStyles';
+import Routes from './routes';
 
 const App: React.FC = () => {
     return (
         <ThemeProvider theme={dark}>
-        <GlobalStyles />
-        <Layout />
+            <GlobalStyles />
+            <Routes />
         </ThemeProvider>
     );
 }
