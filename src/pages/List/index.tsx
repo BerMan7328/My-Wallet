7e7000af7eafbe055ef7c90aefb314b46cc99cbe
BerMan7328 <bmanoelbh@gmail.com>
2021-02-28 14:32:36 -0300

    commit formatcurrency att

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -5,7 +5,7 @@ import SelectInput from '../../components/SelectInput';
 import HistoryFinnanceCard from '../../components/HistoryFinnanceCard';
 import gains from '../../repositories/gains';
 import expenses from '../../repositories/expenses';
-import { isTemplateTail } from 'typescript';
+import formatCurrency from '../../utils/formatCurrency';
 
 interface IRouteParams {
     match: {
@@ -56,7 +56,7 @@ const List: React.FC<IRouteParams> = ({ match }) => {
             return {
                 id: String(Math.random () * data.length),
                 description: item.description,
-                amountFormatted: item.amount,
+                amountFormatted: formatCurrency(Number(item.amount)),
                 frequency: item.frequency,
                 dateFormatted: item.date,
                 tagColor: item.frequency === 'recorrente' ? '#4E41F0' : '#E44C4E'
