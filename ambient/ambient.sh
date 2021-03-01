b2c46cf597ffaf1740bb2a09deb2d766e31b43fd
BerMan7328 <43593704+BerMan7328@users.noreply.github.com>
2021-03-01 18:28:07 -0300

    Update ambient.sh

--- ambient/ambient.sh
+++ ambient/ambient.sh
@@ -13,7 +13,7 @@ docker volume create gitlab_logs &&
 docker volume create gitlab_config &&
 docker volume create gitlab-runner_config &&
 
-curl -L " https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname  -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
+curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
 chmod +x /usr/local/bin/docker-compose &&
 
 yarn add react &&
