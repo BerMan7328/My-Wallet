400bdc3a27300f5ada7d632f8e1bdf181fde88b6
BerMan7328 <43593704+BerMan7328@users.noreply.github.com>
2021-03-01 14:44:12 -0300

    Update ambient.sh

--- ambient/ambient.sh
+++ ambient/ambient.sh
@@ -1,6 +1,7 @@
 yum update -y &&
 yum install -y docker &&
-yum install git &&
+yum install -y git &&
+yum install -y yarn &&
 
 systemctl start docker &&
 systemctl enable docker &&
@@ -14,6 +15,18 @@ docker volume create gitlab-runner_config &&
 curl -L " https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname  -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
 chmod +x /usr/local/bin/docker-compose &&
 
+yarn add react &&
+yarn add node &&
+yarn add styled-components &&
+yarn add react-dom &&
+yarn add react-router-dom &&
+yarn add react-scripts &&
+yarn add react-switch &&
+yarn add typescript &&
+yarn add uuidv4 &&
+yarn add user-event &&
+yarn add jest-dom &&
+
 cd /tmp &&
 git init &&
 git remote add origin https://github.com/BerMan7328/My-Wallet.git &&
@@ -22,6 +35,5 @@ cd /tmp/My-Wallet/ambient &&
 docker-compose up -d &&
 
 rm -rf /tmp/My-Wallet &&
-yum remove git &&
 
 echo "ambiente finalizado, containers ativos"
