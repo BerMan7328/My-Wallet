8659323d96ab5c3580a0791d56cfe8f0f8bcbd25
BerMan7328 <43593704+BerMan7328@users.noreply.github.com>
2021-03-01 08:53:08 -0300

    Update ambient.sh

--- ambient/ambient.sh
+++ ambient/ambient.sh
@@ -7,3 +7,5 @@ systemctl enable docker &&
 docker volume create mysql_data &&
 docker volume create gitlab_data &&
 
+curl -L " https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname  -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
+chmod +x /usr/local/bin/docker-compose &&
