5c107dea83d91b671cf6e4375c86d63383bd0676
BerMan7328 <43593704+BerMan7328@users.noreply.github.com>
2023-03-21 17:16:24 -0300

    Create dark.ts

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,14 @@
+export default {
+  title: 'dark',
+  colors: {
+    primary: '#333',
+    secondary: '#222',
+    tertiary: '#444',
+    white: '#FFF',
+    black: '#000',
+    gray: '#777',
+    success: '#1ED760',
+    info: '#1DA1F2',
+    warning: '#FFA500',
+  },
+};
