6bf116d66fc5a096a32f3bc277b1ae042c1b6265
BerMan7328 <43593704+BerMan7328@users.noreply.github.com>
2023-03-21 17:49:20 -0300

    Update auth.routes.tsx

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'; 
 
-import SignIn from '../pages/SignIn/index';
+import SignIn from '../pages/Sigin';
 
 const AuthRoutes: React.FC = () => (
     <Switch>
